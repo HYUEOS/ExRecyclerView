3d13f38160279c7c4655a589cb8cf1883d520762
이지영 <violentjy@gmail.com>
2018-08-30 13:28:29 +0900

    initialize MainActivity

--- app/src/main/java/com/violentjy/exrecyclerview/MainActivity.java
+++ app/src/main/java/com/violentjy/exrecyclerview/MainActivity.java
@@ -2,12 +2,32 @@ package com.violentjy.exrecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnLinear, btnGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /** initialize views **/
+        btnLinear = findViewById(R.id.btn_linear);
+        btnGrid = findViewById(R.id.btn_grid);
+
+        btnLinear.setOnClickListener(this);
+        btnGrid.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.btn_linear:
+                break;
+            case R.id.btn_grid:
+                break;
+        }
     }
 }
