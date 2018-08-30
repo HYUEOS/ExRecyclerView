d9592def0d00139e75d569cb92ade8bb4893db4a
이지영 <violentjy@gmail.com>
2018-08-30 16:41:45 +0900

    GridActivity 에 DataBinding 적용

--- app/src/main/java/com/jyoung/exrecyclerview/GridActivity.java
+++ app/src/main/java/com/jyoung/exrecyclerview/GridActivity.java
@@ -1,22 +1,23 @@
 package com.jyoung.exrecyclerview;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.jyoung.exrecyclerview.adapter.GridAdapter;
+import com.jyoung.exrecyclerview.databinding.ActivityGridBinding;
+import com.jyoung.exrecyclerview.model.DummyData;
 
 public class GridActivity extends AppCompatActivity {
-    private RecyclerView rvGrid;
+    private ActivityGridBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_grid);
 
-        // initialize views
-        rvGrid = findViewById(R.id.rv_grid);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_grid);
 
         /**
          * LayoutManager 를 설정하지 않으면 다른 걸 다 잘 해도 RecyclerView 가 제대로 뜨지 않습니다!!
@@ -25,10 +26,10 @@ public class GridActivity extends AppCompatActivity {
          * LayoutManager 를 xml 에서 바로 셋팅하는 방법도 있는데 혹시나 궁금하신 분들을 위해 activity_linear.xml 에다 추가해뒀어용
          */
         final GridLayoutManager manager = new GridLayoutManager(this, 2);
-        rvGrid.setLayoutManager(manager);
+        binding.rvGrid.setLayoutManager(manager);
         /** 마찬가지로 adapter 도 까먹으면 안 됨 **/
         final GridAdapter adapter = new GridAdapter();
-        rvGrid.setAdapter(adapter);
+        binding.rvGrid.setAdapter(adapter);
 
         // set header for grid layout manager
         manager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
@@ -37,5 +38,8 @@ public class GridActivity extends AppCompatActivity {
                 return adapter.isHeader(position) ? manager.getSpanCount() : 1;
             }
         });
+
+        // set items
+        binding.setItems(DummyData.items);
     }
 }
