f46eb1de4b276428c08319054c8d24392aa26a58
이지영 <violentjy@gmail.com>
2018-08-30 16:33:56 +0900

    LinearActivity 에 DataBinding 적용

--- app/src/main/java/com/jyoung/exrecyclerview/LinearActivity.java
+++ app/src/main/java/com/jyoung/exrecyclerview/LinearActivity.java
@@ -1,23 +1,22 @@
 package com.jyoung.exrecyclerview;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
 import com.jyoung.exrecyclerview.adapter.LinearAdapter;
+import com.jyoung.exrecyclerview.databinding.ActivityLinearBinding;
 import com.jyoung.exrecyclerview.model.DummyData;
 
 public class LinearActivity extends AppCompatActivity {
-    private RecyclerView rvLinear;
+    private ActivityLinearBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_linear);
 
-        // initialize views
-        rvLinear = findViewById(R.id.rv_linear);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_linear);
 
         /**
          * LayoutManager 를 설정하지 않으면 다른 걸 다 잘 해도 RecyclerView 가 제대로 뜨지 않습니다!!
@@ -25,8 +24,11 @@ public class LinearActivity extends AppCompatActivity {
          * 로그캣에 빨간 줄로 skipping layout 어쩌구 layoutmanager (혹은 adapter) 가 설정되어있지 않다 고 영어로 다 보여줍니당
          * LayoutManager 를 xml 에서 바로 셋팅하는 방법도 있는데 혹시나 궁금하신 분들을 위해 activity_linear.xml 에다 추가해뒀어용
           */
-        rvLinear.setLayoutManager(new LinearLayoutManager(this));
+        binding.rvLinear.setLayoutManager(new LinearLayoutManager(this));
         /** 마찬가지로 adapter 도 까먹으면 안 됨 **/
-        rvLinear.setAdapter(new LinearAdapter());
+        binding.rvLinear.setAdapter(new LinearAdapter());
+
+        // set items
+        binding.setItems(DummyData.items);
     }
 }
